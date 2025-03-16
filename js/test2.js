f4b1ec6aecee39fcbfe2ae42292b4a7e4a750b7f
Fils25git <fleduportal25@gmail.com>
2025-03-16 11:46:30 -0700

    Update test2.js

--- js/test2.js
+++ js/test2.js
@@ -3,7 +3,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Initialize Supabase (Use your actual public key)
 const SUPABASE_URL = "https://lrwqsjxvbyxfaxncxisg.supabase.co";
-const SUPABASE_KEY = "your-supabase-key"; // Use your Supabase key here
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyd3Fzanh2Ynl4ZmF4bmN4aXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0ODI3NzQsImV4cCI6MjA1NzA1ODc3NH0.gpFO3mW2hKRYleTRn3UEU0IgdNsIDgLdttQBnflu2qc"; // Use your Supabase key here
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Select the form and other DOM elements
